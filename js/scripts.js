dc3e962fbfa4d6707a5bff67f3cf210558040713
denchikUniversal <tkachenkodenys94@gmail.com>
2018-08-22 17:48:48 +0300

    1

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+	
+	/* scroll */
+	
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
+	/* timer */
+
+	function update() {
+		var Now = new Date(), Finish = new Date();
+		Finish.setHours( 23);
+		Finish.setMinutes( 59);
+		Finish.setSeconds( 59);
+		if( Now.getHours() === 23  &&  Now.getMinutes() === 59  &&  Now.getSeconds === 59) {
+			Finish.setDate( Finish.getDate() + 1);
+		}
+		var sec = Math.floor( ( Finish.getTime() - Now.getTime()) / 1000);
+		var hrs = Math.floor( sec / 3600);
+		sec -= hrs * 3600;
+		var min = Math.floor( sec / 60);
+		sec -= min * 60;
+		$(".timer .hours").text( pad(hrs));
+		$(".timer .minutes").text( pad(min));
+		$(".timer .seconds").text( pad(sec));
+		setTimeout( update, 200);
+	}
+	function pad(s) { return ('00'+s).substr(-2) }
+	update();
+
+	/* sliders */
+
+	$(".slider").owlCarousel({
+		items: 1,
+		loop: true,
+		smartSpeed: 300,
+		mouseDrag: false,
+		pullDrag: false,
+		dots: false,
+		nav: true,
+		navText: ""
+	});
+
+	$(".reviews_list").owlCarousel({
+		smartSpeed: 300,
+		mouseDrag: false,
+		pullDrag: false,
+		dots: false,
+		navText: "",
+		responsive: {
+			0: {
+				items: 1,
+				margin: 0,
+				nav: true,
+				loop: true
+			},
+			640: {
+				items: 2,
+				margin: 20,
+				nav: true,
+				loop: true
+			},
+			960: {
+				items: 3,
+				margin: 20,
+				nav: false,
+				loop: false
+			}
+		}
+	});
+
+});
